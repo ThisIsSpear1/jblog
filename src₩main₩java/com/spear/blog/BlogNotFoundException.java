9ddd0e2e4423e420c5c8a0fc21a0c21b8504fde7
thisisspear1 <rjsckdd12@gmail.com>
2020-07-08 01:04:17 +0900

    Fix the error and update pom
    Description Resource Path Location Type
    Target runtime Apache Tomcat v8.5 is not defined.
    jblog Unknown Faceted Project Problem
    ---
    alter apache tomcat version and project facets

--- src₩main₩java/com/spear/blog/BlogNotFoundException.java
+++ src₩main₩java/com/spear/blog/BlogNotFoundException.java
@@ -2,6 +2,11 @@ package com.spear.blog;
 
 public class BlogNotFoundException extends RuntimeException {
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public BlogNotFoundException(Long id){
 		super("Could not find Blog" + id);
 	}
